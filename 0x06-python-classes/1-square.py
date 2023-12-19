d3f9e1229300348d8c92672667d9baa283f35733
esrAA312 <esraayahya312@gmail.com>
2023-12-19 02:40:22 -0800

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Constructor.
+            size: length
+        """
+        self.__size = size
