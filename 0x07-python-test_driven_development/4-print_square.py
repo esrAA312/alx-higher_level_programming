573e389fb882857af21d0c1b15c34d286db70b2c
esrAA312 <esraayahya312@gmail.com>
2024-01-03 00:18:07 -0800

    done

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" print_square method."""
+
+
+def print_square(size):
+    """
+    Prints a square using the '#' character.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/4-print_square.txt")
