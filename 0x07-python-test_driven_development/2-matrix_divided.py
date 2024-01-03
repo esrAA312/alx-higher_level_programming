3517a77ff4b83fbe51cc93e0f0d3e2620605d079
esrAA312 <esraayahya312@gmail.com>
2024-01-03 13:11:18 -0800

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,34 +4,23 @@
 
 def matrix_divided(matrix, div):
     """
-    Divides all elements of a matrix.
-
-    Args:
-    matrix (list of lists): The matrix containing integers or floats.
-    div (int or float): The number to divide the matrix elements by.
-
-    Returns:
-    list of lists: A new matrix with elements rounded to 2 decimal places.
-
-    Raises:
     TypeError: If matrix is not a list of lists of integers or floats.
     TypeError: If each row of the matrix does not have the same size.
     TypeError: If div is not a number (integer or float).
-    ZeroDivisionError: If div is equal to 0.
-    """
+    ZeroDivisionError: If div is equal to 0."""
 
     # Validate matrix
     for row in matrix:
         if not isinstance(row, list) or len(row) == 0:
             raise TypeError(
-                "matrix must be a matrix (list of lists) " + "of int/float"
+                "matrix must be a matrix (list of lists) " + "of integers/floats"
             )
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for x in row:
             if not isinstance(x, (int, float)):
                 raise TypeError(
-                    "matrix must be a matrix (list of lists) " + "of int/float"
+                    "matrix must be a matrix (list of lists) " + "of integers/floats"
                 )
 
     if not isinstance(matrix, list) or len(matrix) == 0:
@@ -44,9 +33,7 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
 
     # Perform matrix division and round to 2 decimal places
-    result_matrix = [
-        [round(element / div, 2) for element in row] for row in matrix]
-
+    result_matrix = [[round(element / div, 2) for element in row] for row in matrix]
     return result_matrix
 
 
