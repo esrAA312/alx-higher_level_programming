63981dd2b44854c9f6659c22846fdd60ca60570b
esrAA312 <esraayahya312@gmail.com>
2024-01-09 02:44:37 -0800

    done

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,15 +3,12 @@
 
 
 class Student:
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-
-
         try:
             for attr in attrs:
                 if type(attr) is not str:
@@ -25,7 +22,6 @@ class Student:
         return my_dict
 
     def reload_from_json(self, json):
-
         for k, v in json.items():
             if k in self.__dict__:
-                self.__dict__[k] = v  
+                self.__dict__[k] = v
