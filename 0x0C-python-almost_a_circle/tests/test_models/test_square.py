26b64886550d76038d7ee1bc051305407f5254d9
esrAA312 <esraayahya312@gmail.com>
2024-01-15 15:22:31 -0800

    done

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -85,7 +85,53 @@ class TestSquare(unittest.TestCase):
             Square.display()
         p = "display() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), p)
+    def test_nan_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, float('nan'), 20)
 
+    def test_complex_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(7, 36, complex(45))
+
+    def test_dict_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(16, 10, {"a": 1, "b": 2})
+
+    def test_list_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(16, 1, [10, 20, 3])
+
+    def test_set_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 107, {16, 2, 73})
+
+    def test_tuple_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 1, (8, 2, 93))
+
+    def test_frozenset_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 3, frozenset({17, 26, 3, 1}))
+
+    def test_range_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 3, range(5))
+
+    def invalid_types(self):
+        n = (3.1, -1.51, float('inf'), float('-inf'), True, "hi", (4,),
+             [4], {4}, {9: 77}, None)
+        return n
+
+    def test_dict_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, {"a": 4, "b": 5}, 6)
+
+    def test_display(self):
+        m = Square(10)
+        with self.assertRaises(TypeError) as e:
+            Square.display()
+        n = "display() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), n)
 if __name__ == "__main__":
     unittest.main()
 			
