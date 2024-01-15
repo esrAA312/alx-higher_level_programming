d467144135f8f38fbf7063dfe5f8eea4f9cfc3e6
esrAA312 <esraayahya312@gmail.com>
2024-01-15 15:19:02 -0800

    done

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -20,6 +20,60 @@ class TestSquare(unittest.TestCase):
     def test_list_size(self):
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
             Square([8, 7, 6])
+    def test_display_s(self):
+        m = Square(1)
+        n = io.StringIO()
+        with redirect_stdout(n):
+            m.display()
+        p = "#\n"
+        self.assertEqual(n.getvalue(), p)
+
+        m.size = 2
+        n = io.StringIO()
+        with redirect_stdout(n):
+            m.display()
+        p = "\
+##\n\
+##\n\
+"
+        self.assertEqual(n.getvalue(), p)
+
+        m = Square(5, 5, 5)
+        f = io.StringIO()
+        with redirect_stdout(f):
+            m.display()
+        p = """\
+
+
+
+
+
+     #####
+     #####
+     #####
+     #####
+     #####
+"""
+        self.assertEqual(f.getvalue(), p)
+
+        m = Square(8, 8)
+        f = io.StringIO()
+        with redirect_stdout(f):
+            m.display()
+        n = """\
+        ########
+        ########
+        ########
+        ########
+        ########
+        ########
+        ########
+        ########
+"""
+        self.assertEqual(f.getvalue(), n)
+
+    def test_B_inheritance(self):
+        self.assertTrue(issubclass(Square, Base))
 
     def test_set_size(self):
         with self.assertRaisesRegex(TypeError, "width must be an integer"):
