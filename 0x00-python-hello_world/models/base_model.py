00187212dce78c30976700e547df996d700f1f48
esrAA312 <esraayahya312@gmail.com>
2024-02-01 11:30:04 -0800

    done

--- 0x00-python-hello_world/models/base_model.py
+++ 0x00-python-hello_world/models/base_model.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """This is the base class"""
 
-
+import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """ Base"""
+    """Base"""
+
     def __init__(self, *myargs, **mykwargs):
-        """ init """
-        ISO = '%Y-%m-%dT%H:%M:%S.%f'
+        """init"""
+        ISO = "%Y-%m-%dT%H:%M:%S.%f"
         if mykwargs is not None and mykwargs != {}:
             for key, value in mykwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -22,27 +23,29 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
-        """ save method """
+        """save method"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """To dict method"""
-        ISO = '%Y-%m-%dT%H:%M:%S.%f'
+        ISO = "%Y-%m-%dT%H:%M:%S.%f"
 
         iso_format_keys = {"created_at", "updated_at"}
 
-        dict_copy = {key: value.isoformat() if key in iso_format_keys and hasattr(value, 'isoformat') else value
-                     for key, value in self.__dict__.items()}
-        
-        dict_copy['__class__'] = self.__class__.__name__
-        
+        dict_copy = {
+            key: value.isoformat()
+            if key in iso_format_keys and hasattr(value, "isoformat")
+            else value
+            for key, value in self.__dict__.items()
+        }
+        dict_copy["__class__"] = self.__class__.__name__
         return dict_copy
 
-        
     def __str__(self):
-        """ __str__ method """
+        """__str__ method"""
         className = self.__class__.__name__
         return "[{}] ({}) {}".format(className, self.id, self.__dict__)
-
