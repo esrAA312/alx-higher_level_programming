c878d2a90889bb0b0526b2d76c111a035b2920b4
esrAA312 <esraayahya312@gmail.com>
2024-02-14 05:56:26 -0800

    done

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
--- lists all genres from hbtn_0d_tvshows and displays the number ,rows of a database meeting a condition
+-- lists all rows of a database meeting a condition
 SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
-FROM tv_genres LEFT JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id
+FROM tv_show_genres LEFT JOIN tv_genres
+ON  tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
