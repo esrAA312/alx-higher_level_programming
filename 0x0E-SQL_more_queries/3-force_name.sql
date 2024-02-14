d6bb74345d9f76fcf3ae7d0e807957ca4c278baf
esrAA312 <esraayahya312@gmail.com>
2024-02-14 02:59:14 -0800

    done

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
--- Create or update the force_name table if it does not exist
+-- Create or update the force_name table
 
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
