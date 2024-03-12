6cc719572bbdbca2b96a034833fb6036ea9dfb87
esrAA312 <esraayahya312@gmail.com>
2024-03-12 04:16:42 -0700

    done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 const SquareP = require('./5-square');
 
 module.exports = class Square extends SquareP {
@@ -6,16 +5,11 @@ module.exports = class Square extends SquareP {
     if (c === undefined) {
       c = 'X';
     }
-    let i = 0;
-    while (i < this.height) {
-      let s = '';
-      let j = 0;
-      while (j < this.width) {
-        s += c;
-        j++;
-      }
-      console.log(s);
-      i++;
+    let squareStr = '';
+    for (let i = 0; i < this.height; i++) {
+      const row = c.repeat(this.width);
+      squareStr += row + '\n';
     }
+    console.log(squareStr);
   }
 };
