c3e9fdab409ed85a20898675956a65656da67d4c
esrAA312 <esraayahya312@gmail.com>
2024-03-15 10:26:00 -0700

    done

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
