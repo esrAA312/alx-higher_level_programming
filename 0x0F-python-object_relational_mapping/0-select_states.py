fdc6a39ed7bef2f977b9b72adb2d424eed2475e5
esrAA312 <esraayahya312@gmail.com>
2024-03-15 01:48:14 -0700

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,11 +17,12 @@ def list_states(username, password, database):
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY states. id")
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
 
     rows = cursor.fetchall()
     [print(row) for row in rows]
 
+    cursor.close()
     db.close()
 
 
