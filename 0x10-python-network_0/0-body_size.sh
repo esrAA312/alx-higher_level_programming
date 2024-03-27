8120c44e3acbfd672599b911779f04c4775d3152
esrAA312 <esraayahya312@gmail.com>
2024-03-27 02:13:35 -0700

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | awk '/Content-Length:/ {print $2}' && [ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1
+curl -sI "${1}" | wc -c
