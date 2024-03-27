817d2dbd7c2c35dac9df928525b4ec70f0d5c90c
esrAA312 <esraayahya312@gmail.com>
2024-03-27 02:32:54 -0700

    done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL, and displays the body of the response
-curl -s -L "${1}"
+# Sends a GET request to the URL, and displays the body 
+curl -s -o /dev/stdout -w "%{http_code}" "$1" | awk 'NR==1 && $1==200 {next} {print}'
