66e5a0e51eabebd61b9a44d9c8ff794d85a9c31a
esrAA312 <esraayahya312@gmail.com>
2024-03-30 04:57:49 -0700

    done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     Ve = {"email": sys.argv[2]}
     De = urllib.parse.urlencode(Ve)
-    De = data.encode("ascii")
+    De = De.encode("ascii")
     request = urllib.request.Request(url, De)
     with urllib.request.urlopen(request) as ree:
         Xe = ree.read().decode("utf-8")
