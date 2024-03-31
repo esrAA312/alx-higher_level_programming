20a9d0aad564235c35d57684f019fb45dcb5e42d
esrAA312 <esraayahya312@gmail.com>
2024-03-31 08:02:36 -0700

    done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ from sys import argv
 def getide(Ue, Pe):
     Ue = f"https://api.github.com/user"
     headers = {"Authorization": f"token {Pe}"}
-    response = requests.get(U, headers=headers)
+    response = requests.get(Ue, headers=headers)
     if response.status_code == 200:
         uu = response.json()
         if "id" in uu:
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     ne = argv[1]
     Pe = argv[2]
 
-    ide = getide(Ue, Pe)
+    ide = getide(ne, Pe)
     print(ide)
