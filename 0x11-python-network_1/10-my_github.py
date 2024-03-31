1ea5a695720dae2b14047895643f4e1b2069515a
esrAA312 <esraayahya312@gmail.com>
2024-03-31 04:56:38 -0700

    done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""github."""
-
+"""github"""
 import requests
 import sys
 
 
 def get_user_id(username, password):
-    url = f"https://api.github.com/users/{username}"
-    response = requests.get(url, auth=(username, password))
-    user_data = response.json()
-    return user_data.get('id')
+    url = f"https://api.github.com/user"
+    headers = {"Authorization": f"token {password}"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        return user_data["id"]
+    else:
+        return None
 
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
+
     user_id = get_user_id(username, password)
     print(user_id)
