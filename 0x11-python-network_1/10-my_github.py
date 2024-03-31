1f00559d46b08a07d0e9b47cb5021b5585b1271b
esrAA312 <esraayahya312@gmail.com>
2024-03-31 04:39:43 -0700

    done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+
+def get_user_id(username, password):
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, auth=(username, password))
+    user_data = response.json()
+    return user_data.get('id')
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    user_id = get_user_id(username, password)
+    print(user_id)
